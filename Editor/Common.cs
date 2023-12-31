8cf23300015a3ec40a72ae303f0994e502a05aa6
ribeKim <ribe@ribe.io>
2023-12-31 20:17:57 +0900

    refac: 공통 코드 따로 뺌

---
+++ Editor/Common.cs
@@ -0,0 +1,37 @@
+﻿using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+namespace Ribe.UnityAnimTool
+{
+
+    public static class Common
+    {
+        public static void SaveAnimationClip(AnimationClip animationClip, string clipName, string folder)
+        {
+            var savePath = Path.Combine(folder, clipName).Replace("\\", "/");
+            if (File.Exists(savePath)) File.Delete(savePath);
+
+            AssetDatabase.CreateAsset(animationClip, savePath);
+            AssetDatabase.Refresh();
+        }
+
+        public static string GetAnimationPath(GameObject gameObject)
+        {
+            var parent = gameObject.transform.parent;
+            var path = gameObject.name;
+
+            while (parent != null)
+            {
+                if (parent.parent != null)
+                {
+                    path = parent.name + "/" + path;
+                }
+
+                parent = parent.parent;
+            }
+
+            return path;
+        }
+    }
+}

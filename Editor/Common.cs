7e3f2da83195b259999f752ed693938233248464
ribeKim <ribe@ribe.io>
2024-01-19 21:12:48 +0900

    refac: 줄바꿈 같은거 변경함

--- Editor/Common.cs
+++ Editor/Common.cs
@@ -6,23 +6,24 @@ using UnityEngine;
 
 namespace Ribe.UnityAnimTool
 {
-
     public static class Common
     {
         public static string POIYOMI_MAX_LIGHT = "material._LightingCap";
         public static string POIYOMI_MIN_LIGHT = "material._LightingMinLightBrightness";
+
         public static string[] POIYOMI_GRAY_LIGHT =
         {
             "material._LightingMonochromatic",
             "material._LightingAdditiveMonochromatic"
         };
+
         public static string POIYOMI_SHADOW_LIGHT = "material._ShadowStrength";
-        
+
         public static string LILTOON_MAX_LIGHT = "material._LightMaxLimit";
         public static string LILTOON_MIN_LIGHT = "material._LightMinLimit";
         public static string LILTOON_MONO_LIGHT = "material._MonochromeLighting";
         public static string LILTOON_UNLIT_LIGHT = "material._AsUnlit";
-        
+
         public static void SaveAnimationClip(AnimationClip animationClip, string clipName, string folder)
         {
             var savePath = Path.Combine(folder, clipName).Replace("\\", "/");
@@ -31,14 +32,13 @@ namespace Ribe.UnityAnimTool
             AssetDatabase.CreateAsset(animationClip, savePath);
             AssetDatabase.Refresh();
         }
-        
-        
+
         public static AnimationClip CreateLightAnimationClip(IEnumerable<GameObject> objects, string propertyName)
         {
             var animationClip = new AnimationClip();
             var curve = new AnimationCurve(new Keyframe(0.0f, 0.0f), new Keyframe(0.01f, 1.0f));
 
-            foreach (var animationPath in objects.Select(Common.GetAnimationPath))
+            foreach (var animationPath in objects.Select(GetAnimationPath))
             {
                 animationClip.SetCurve(animationPath, typeof(SkinnedMeshRenderer), propertyName, curve);
             }
@@ -51,7 +51,7 @@ namespace Ribe.UnityAnimTool
             var animationClip = new AnimationClip();
             var curve = new AnimationCurve(new Keyframe(0.0f, 0.0f), new Keyframe(0.01f, 1.0f));
 
-            foreach (var animationPath in objects.Select(Common.GetAnimationPath))
+            foreach (var animationPath in objects.Select(GetAnimationPath))
             {
                 foreach (var s in propertyName)
                 {
